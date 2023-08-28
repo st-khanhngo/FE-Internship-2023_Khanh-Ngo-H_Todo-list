ff073c6b207274c6ddeeb81c650c1fac5ac49b08
st-khanhngo <khanh.ngo@supremetech.vn>
2023-08-28 14:00:03 +0700

    fix indentation tab space

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,11 @@ import Home from './app/pages/Home';
 import './app/stylesheets/style.scss';
 
 function App() {
-	return (
-		<div className='App'>
-			<Home />
-		</div>
-	);
+  return (
+    <div className='App'>
+      <Home />
+    </div>
+  );
 }
 
 export default App;
